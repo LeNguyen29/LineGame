6f19010985bd167e3b9488bd324c2a5f953d7bbe
LeNguyen2k <nguyenbale29@gmail.com>
2022-05-07 12:50:05 +0700

    Add Game Icon and made a Build for Windows

--- Assets/Scripts/Camera/CameraZoom.cs
+++ Assets/Scripts/Camera/CameraZoom.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class CameraZoom : MonoBehaviour
 {
     public GridHandler gridHandler;
+    public float offset;
 
     private void Start()
     {
+        int width, height;
+
+        gridHandler.getGridSize(out width, out height);
+
         // Camera orthographic size base on map width
         // float orthoSize = sprite.bounds.size.x * Screen.height / Screen.width * 0.5f;
 
@@ -16,21 +21,17 @@ public class CameraZoom : MonoBehaviour
 
         //Camera.main.orthographicSize = orthoSize;
 
-        int width, height;
-
-        gridHandler.getGridSize(out width, out height);
-
         float screenRatio = Screen.width / Screen.height;
         float targetRatio = width / height;
 
         if (screenRatio >= targetRatio)
         {
-            Camera.main.orthographicSize = height / 2;
+            Camera.main.orthographicSize = height / 2 + offset;
         }
         else
         {
             float differenceInSize = targetRatio / screenRatio;
-            Camera.main.orthographicSize = height / 2 * differenceInSize;
+            Camera.main.orthographicSize = height / 2 * differenceInSize + offset;
         }
     }
 }
